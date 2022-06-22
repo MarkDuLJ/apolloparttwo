36e6d280f5476bbc4ae89341eceaee476abd7ddb
Mark Du <dulongjie@gmail.com>
2022-06-21 21:30:47 -0400

    compare datasources with fetch

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,14 +1,21 @@
+const fetch = require("node-fetch");
+
 const resolvers = {
   Query: {
     //returns an array of tracks
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    tracksForHomeFetch: async () => {
+      const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+      const res = await fetch(`${baseUrl}/tracks`);
+      return res.json();
+    },
   },
 
   Track: {
     author: (parent, _, { dataSources }) => {
-      console.log("Parent parameter is: ", parent);
+      //   console.log("Parent parameter is: ", parent);
       return dataSources.trackAPI.getAuthor(parent.authorId);
     },
   },
